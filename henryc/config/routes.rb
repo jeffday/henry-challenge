82000bf2f97423843b8e32b66c74787cbbaccb37
jeffday <jeffday@gmail.com>
2023-11-19 11:08:56 -0600

    rails 7 ships with a bunch of stuff by default i don't need for this, strip it all out so our API just reflects the paths we want

--- henryc/config/routes.rb
+++ henryc/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :reservations
+  resources :reservations do
+    post 'reserve', on: :member
+  end
+
   resources :openings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

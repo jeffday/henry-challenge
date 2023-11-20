e5c6827ad5c9a7bcb523a66f6536c5fb8091c599
jeffday <jeffday@gmail.com>
2023-11-19 23:53:08 -0600

    validate the opening fits at least one appointment

--- henryc/app/models/opening.rb
+++ henryc/app/models/opening.rb
@@ -1,12 +1,8 @@
 class Opening < ApplicationRecord
-    # TODO: validation that opening is long enough to accommodate at least one appointment
+    validate :fits_at_least_one_appointment
 
     def create_appointments
-        window = self.end - self.start
-
-        number_of_appointments = window / (Appointment::APPOINTMENT_LENGTH)
-
-        end_offset = number_of_appointments.floor - 1
+        end_offset = number_of_appointments - 1
 
         # TODO: error handling if appointment creation fails
         (0..end_offset).each do |i|
@@ -17,5 +13,19 @@ class Opening < ApplicationRecord
 
             Appointment.create(provider_id: self.provider_id, start_time: start_time, end_time: end_time)
         end
+    end
+
+    private
+
+    def duration
+        self.end - self.start
+    end
+
+    def number_of_appointments
+        (duration / Appointment::APPOINTMENT_LENGTH).floor
+    end
+
+    def fits_at_least_one_appointment
+        errors.add(:base, "The duration of the opening needs to fit at least one appointment.") unless number_of_appointments >= 1
     end
 end
--- henryc/test/models/opening_test.rb
+++ henryc/test/models/opening_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class OpeningTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @provider = providers(:one)
+  end
+
+  test "creating an opening shorter than the minimum appointment time should fail validation" do
+    start_time = DateTime.new(2023,11,20,9,0,0)
+    end_time = DateTime.new(2023,11,20,9,10,0)
+
+    record = Opening.create(provider_id: @provider.id, start: start_time, end: end_time)
+
+    assert_equal record.errors[:base].length, 1, "there should be a validation error on the base object"
+  end
+
+  test "creating an opening longer than the minimum appointment time should pass validation" do
+    start_time = DateTime.new(2023,11,20,9,0,0)
+    end_time = DateTime.new(2023,11,20,9,15,0)
+
+    record = Opening.create(provider_id: @provider.id, start: start_time, end: end_time)
+
+    assert_equal record.errors[:base].length, 0, "there should not be a validation error on the base object"
+  end
 end
